8658f80bf37d6054b66378bdadfb6e011fe4ca93
trygvevang <trygvevang@Trygves-MacBook-Pro.local>
2018-03-27 11:52:44 +0200

    Init commit creating an app with empty view fixed to the size of scene

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    int w = 800, h = 600;
+    scene->setSceneRect(0, 0, w, h);
+
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->setFixedSize(w, h);
+    view->show();
+
+    return a.exec();
+}
