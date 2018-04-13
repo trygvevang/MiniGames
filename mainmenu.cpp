51f19a4e43e1ff25283b4cf05e6b0cabe7944809
trygvevang <trygve.n.vang@gmail.com>
2018-04-13 13:19:54 +0200

    removing duplicate member function call

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -12,7 +12,6 @@ MainMenu::MainMenu(QWidget *parent) : QWidget(parent), ui(new Ui::MainMenuUi)
     connect(ui->exitButton,           SIGNAL(clicked()), this, SLOT(handleExit()));
     connect(ui->button2048,           SIGNAL(clicked()), this, SLOT(handle2048()));
     QObject::connect(tetrisGame, SIGNAL(gameClosed()), this, SLOT(showMainMenu()));
-    setSettings();
 }
 
 // Get highscores for Tetris and 2048
